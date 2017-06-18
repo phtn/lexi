33b4c04b251698b537b1fbf1d9e02a1bcdad1b8f
phtn <phtn458@gmail.com>
2017-06-18 04:44:18 -0400

    icons in info

--- src/components/info.js
+++ src/components/info.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Slider from 'react-slick'
+import { Icon } from 'semantic-ui-react'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
@@ -20,15 +21,20 @@ const slides = {
   fontWeight: 100,
   fontSize: 18
 }
+const icon = {
+  marginRight: 10,
+  fontSize: 12,
+  color: '#ccc'
+}
 
 export default props => (
   <div style={container} className='animated fadeInUp'>
     <Slider {...settings}>
-      <div style={slides}>Wil Medina</div>
-      <div style={slides}>Software Engineer</div>
-      <div style={slides}>Philadelphia, PA</div>
-      <div style={slides}>@phtn458</div>
-      <div style={slides}>github.com/phtn</div>
+      <div style={slides}><Icon style={icon} name='address card'/>Wil Medina</div>
+      <div style={slides}><Icon style={icon} name='puzzle'/>Software Engineer</div>
+      <div style={slides}><Icon style={icon} name='map pin'/>Philadelphia, PA</div>
+      <div style={slides}><Icon style={icon} name='twitter'/>@phtn458</div>
+      <div style={slides}><Icon style={icon} name='github'/>github.com/phtn</div>
     </Slider>
   </div>
 )
